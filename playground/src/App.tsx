7cb110b5e6f268919323307638f880c17b22d06f
Scofez <dridinassim9@gmail.com>
2026-01-02 16:41:26 +0100

    tsconfig-enhance

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,6 +1,6 @@
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import { AuthForm, type AuthCredentials } from '../../src/index';
+import { AuthForm, type AuthCredentials } from '@scofez/easy-auth';
 
 function App() {
   const handleLogin = (creds: AuthCredentials) => {
